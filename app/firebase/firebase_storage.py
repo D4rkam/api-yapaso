3b19da40983e4dc5b7c6126021ca94859fba7f3a
D4rkam <thomaslinares32@gmail.com>
2024-07-29 22:40:09 -0300

    v1.0

--- app/firebase/firebase_storage.py
+++ app/firebase/firebase_storage.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, storage
 import os
 
-path = os.path.join(os.path.dirname(__file__), 'serviceAccountKey.json')
+path = os.path.join(os.path.dirname(__file__), '.serviceAccountKey.json')
 
 cred = credentials.Certificate(path)
 firebase_admin.initialize_app(
