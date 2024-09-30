71d2b81146c90ea21d10676b7e12a70f5168886a
ElpibeInformatico <thomaslinares32@gmail.com>
2024-09-30 12:31:00 -0300

    acceso segun el rol

--- app/controllers/order_controller.py
+++ app/controllers/order_controller.py
@@ -5,6 +5,9 @@ from dependencies import db_dependency
 from schemas.order_schema import OrderCreate, Order
 from services.order_service import create_user_order, get_orders
 from dependencies import user_dependency
+from security import verify_roles
+from models.user_model import User
+from fastapi import Depends
 
 
 router = APIRouter(
@@ -14,10 +17,10 @@ router = APIRouter(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_order_for_user(order: OrderCreate, db: db_dependency, current_user: user_dependency):
+async def create_order_for_user(order: OrderCreate, db: db_dependency, current_user: User = Depends(verify_roles(["user"]))):
     return create_user_order(db=db, order=order, user_id=current_user.id)
 
 
 @router.get("/", response_model=List[Order])
-async def get_orders_user(db: db_dependency, user: user_dependency):
+async def get_orders_user(db: db_dependency, current_user: User = Depends(verify_roles(["seller"]))):
     return get_orders(db)
--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -3,6 +3,9 @@ from dependencies import db_dependency, user_dependency
 from services.product_service import get_products, delete_product, get_product_by_id, create_product_db, upload_image
 from typing import List
 from schemas.product_schema import Product, ProductCreate
+from security import verify_roles
+from models.user_model import User
+from fastapi import Depends
 
 
 router = APIRouter(
@@ -12,20 +15,20 @@ router = APIRouter(
 
 
 @router.get("/{product_id}", response_model=Product)
-async def get_product(db: db_dependency, user: user_dependency, product_id: int):
+async def get_product(db: db_dependency, product_id: int, current_user: User = Depends(verify_roles(["user", "seller"]))):
     return get_product_by_id(db, product_id)
 
 
 @router.get("/all", response_model=List[Product])
-async def get_products_list(db: db_dependency, user: user_dependency):
+async def get_products_list(db: db_dependency, current_user: User = Depends(verify_roles(["seller"]))):
     return get_products(db)
 
 
 @router.post("/", response_model=Product)
-async def create(db: db_dependency, user: user_dependency, product_model: ProductCreate, file: UploadFile = File(...)):
+async def create(db: db_dependency, product_model: ProductCreate, file: UploadFile = File(...), current_user: User = Depends(verify_roles(["seller"]))):
     return create_product_db(db, product_model)
 
 
 @router.delete("/{product_id}", status_code=status.HTTP_200_OK)
-async def delete(db: db_dependency, user: user_dependency, product_id: int):
+async def delete(db: db_dependency, product_id: int, current_user: User = Depends(verify_roles(["seller"]))):
     return delete_product(db, product_id)
