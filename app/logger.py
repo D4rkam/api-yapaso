da89a151dade6682d7b0f56f960594ca5841088c
D4rkam <thomaslinares32@gmail.com>
2024-12-24 17:45:38 -0300

    cambio desactivar log watchfiles

--- app/logger.py
+++ app/logger.py
@@ -3,6 +3,9 @@ import logging
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(name)s  - %(levelname)s - %(message)s",
-                    handlers=[logging.FileHandler("access.log"), logging.StreamHandler()])
+                    #      handlers=[logging.FileHandler("access.log"), logging.StreamHandler()]
+                    )
+
+logging.getLogger("watchfiles.main").disabled = True
 
 access_logger = logging.getLogger("AccessLogger")
