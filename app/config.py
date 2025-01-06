db3783fac85752c8ef852cb264ceb32a2948a2c5
D4rkam <thomaslinares32@gmail.com>
2025-01-06 13:10:52 -0300

    cambios configuracion y acceso a la base de datos

--- app/config.py
+++ app/config.py
@@ -1,36 +1,37 @@
-import os
-from dataclasses import dataclass
-from dotenv import load_dotenv
-from decouple import config
+from functools import lru_cache
 
-load_dotenv()
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-@dataclass
-class Settings():
-    API_V1_STR: str = config(
-        "API_V1_STR", cast=str, default="/api")
-    PROJECT_NAME: str = config(
-        "PROJECT_NAME", cast=str, default="Ya Paso")
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file="app/.env", env_file_encoding="utf-8")
+    API_V1_STR: str = "/api"
+    PROJECT_NAME: str = "Ya Paso"
+    APP_DEBUG: bool = False
 
-    DB_CONNECTION: str = config(
-        "DB_CONNECTION",
-        cast=str,
-        default="mysql"
-    )
-    DB_HOST: str = config("DB_HOST", cast=str, default="localhost")
-    DB_PORT: int = config("DB_PORT", default=3306, cast=int)
-    DB_DATABASE: str = config("DB_DATABASE", cast=str, default="api_test")
-    DB_USER: str = config("DB_USERNAME", cast=str, default="root")
-    DB_PASSWORD: str = config("DB_PASSWORD", cast=str, default="")
+    DB_CONNECTION: str = "mysql"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 3306
+    DB_DATABASE: str = "api_test"
+    DB_USER: str = "root"
+    DB_PASSWORD: SecretStr = ""
 
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
+    SECRET_KEY: SecretStr = "supersecretkey"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    ACCESS_TOKEN_MP = os.getenv("MERCADO_PAGO_TOKEN")
 
-    class Config:
-        case_sensitive = True
+    MERCADO_PAGO_TOKEN: SecretStr = ""
+
+    # class Config:
+    #     env_file = "app/.env"  # Asegura que cargue el archivo .env
+    #     env_file_encoding = "utf-8"
 
 
-settings = Settings()
+@lru_cache
+def get_settings() -> Settings:
+    """
+    Carga las configuraciones usando Pydantic.
+    Utiliza lru_cache para evitar recrear instancias innecesarias.
+    """
+    return Settings()
--- app/database.py
+++ app/database.py
@@ -1,24 +1,48 @@
-from sqlalchemy import create_engine, URL
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.config import settings
+from typing import Generator
 
-user = settings.DB_USER
-password = settings.DB_PASSWORD
-host = settings.DB_HOST
-port = settings.DB_PORT
-database = settings.DB_DATABASE
+from sqlalchemy import URL, create_engine
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
+from app.config import get_settings
 
-SQLALCHEMY_DATABASE_URL = URL.create(
-    drivername="mysql",
-    username=user,
-    password=password,
-    host=host,
-    port=port,
-    database=database
-)
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_engine():
+    settings = get_settings()
+    SQLALCHEMY_DATABASE_URL = URL.create(
+        drivername="mysql",
+        username=settings.DB_USER,
+        password=settings.DB_PASSWORD.get_secret_value(),
+        host=settings.DB_HOST,
+        port=settings.DB_PORT,
+        database=settings.DB_DATABASE,
+    )
+    return create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+def get_session_local():
+    engine = get_engine()
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    return Session()
+
+
+def get_db() -> Generator[Session, None, None]:
+    """
+    Dependencia para obtener una sesión de base de datos.
+    Abre una conexión a la base de datos y la cierra después de usarla.
+    """
+    db = get_session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    Base.metadata.create_all(bind=get_engine())
+
+
+def drop_db():
+    Base.metadata.drop_all(bind=get_engine())
+    Base.metadata.drop_all(bind=get_engine())
