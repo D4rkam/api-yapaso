5c54e67dd6ad2ae12ea3a17d5ee237a721e1d743
D4rkam <thomaslinares32@gmail.com>
2025-01-06 13:25:35 -0300

    cambios obtener secret_key

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,10 +1,12 @@
-from sqlalchemy.orm import Session
-from app.models.user_model import User
-from app.models.seller_model import Seller
-from app.security import bcrypt_context
 from datetime import datetime, timedelta
+
 from jose import jwt
-from app.config import settings
+from sqlalchemy.orm import Session
+
+from app.dependencies.settings import settings_dependency
+from app.models.seller_model import Seller
+from app.models.user_model import User
+from app.security import bcrypt_context
 
 
 def authenticate_user(username: str, password: str, db: Session):
@@ -25,15 +27,23 @@ def authenticate_seller(email: str, password: str, db: Session):
     return seller
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+def create_access_token(
+    username: str, user_id: int, expires_delta: timedelta, settings: settings_dependency
+):
     encode = {"sub": username, "id": user_id}
     expires = datetime.utcnow() + expires_delta
     encode.update({"exp": expires})
-    return jwt.encode(encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return jwt.encode(
+        encode, settings.SECRET_KEY.get_secret_value(), algorithm=settings.ALGORITHM
+    )
 
 
-def create_access_token_seller(email: str, seller_id: int, expires_delta: timedelta):
+def create_access_token_seller(
+    email: str, seller_id: int, expires_delta: timedelta, settings: settings_dependency
+):
     encode = {"sub": email, "id": seller_id}
     expires = datetime.utcnow() + expires_delta
     encode.update({"exp": expires})
-    return jwt.encode(encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return jwt.encode(
+        encode, settings.SECRET_KEY.get_secret_value(), algorithm=settings.ALGORITHM
+    )
