a93b370a7e62d57fdb145323df753da38d505c77
D4rkam <thomaslinares32@gmail.com>
2025-01-07 18:30:30 -0300

    cambios de dict al modelo correspondiente

--- app/dependencies/security.py
+++ app/dependencies/security.py
@@ -1,6 +1,10 @@
-from fastapi import Depends
 from typing import Annotated
+
+from fastapi import Depends
+
+from app.models.seller_model import Seller
+from app.models.user_model import User
 from app.security import get_current_seller, get_current_user
 
-user_dependency = Annotated[dict, Depends(get_current_user)]
-seller_dependency = Annotated[dict, Depends(get_current_seller)]
+user_dependency = Annotated[User, Depends(get_current_user)]
+seller_dependency = Annotated[Seller, Depends(get_current_seller)]
